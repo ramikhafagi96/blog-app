09eea7d9fb5b248da8676211a178ae127b30ca0d
ramikh96 <ramikh.developer@gmail.com>
2020-08-28 18:44:08 +0200

    initialize project

---
+++ db/migrate/20200828164107_add_timestamps_to_articles.rb
@@ -0,0 +1,7 @@
+class AddTimestampsToArticles < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
