04b8d66a30f718f56eadd5422c55022cc78afde4
ramikh96 <ramikh.developer@gmail.com>
2020-09-06 19:23:21 +0200

    show user info in index and show views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(whitlist_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article Was Created Successfully"
       redirect_to @article   # redirect_to article_path(@article)
