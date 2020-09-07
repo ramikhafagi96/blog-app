39fdae93926038b54c9cdd9752af323b7997b7a4
ramikh96 <ramikh.developer@gmail.com>
2020-09-07 18:20:19 +0200

    user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController <  ApplicationController
    def new
     @user = User.new
    end
+
+   def create
+    @user = User.new(whitlist_params)
+    if @user.save
+        flash[:notice] = "Welcome To Blog Hub #{@user.username}"
+        redirect_to articles_path
+    else
+        render 'new'
+    end
+   end
+
+   private
+
+   def whitlist_params
+    params.require(:user).permit(:username, :email, :password)
+   end
 end
