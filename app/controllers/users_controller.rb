9e07f1e32a5395a75357883066909dd102b256d5
ramikh96 <ramikh.developer@gmail.com>
2020-09-07 18:33:30 +0200

    edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,21 @@ class UsersController <  ApplicationController
     end
    end
 
+   def edit
+    @user = User.find(params[:id])
+   end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update(whitlist_params)
+        flash[:notice] = "Your Account Information Were Successfully Updated"
+        redirect_to articles_path
+    else
+        render 'edit'
+    end
+   end
+
+
    private
 
    def whitlist_params
