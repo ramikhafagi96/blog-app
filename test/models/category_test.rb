faa1be0db8f9273f6c7c9fe88ebbf97c6d741601
ramikh96 <ramikh.developer@gmail.com>
2020-09-12 23:01:43 +0200

    test for category name field is unique

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -14,5 +14,11 @@ class CategoryTest < ActiveSupport::TestCase
     @category.name = ""
     assert_not @category.valid?
   end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
   
 end
